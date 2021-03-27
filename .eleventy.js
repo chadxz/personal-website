eb9a4bf49bca2e5f45bf1b064aad27169bb09a9a
Chad McElligott <chad.mcelligott@gmail.com>
2021-03-27 18:51:03 -0500

    Add RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
 'use strict';
 const { format } = require('date-fns');
-module.exports = function(eleventyConfig) {
-  eleventyConfig.markdownTemplateEngine = "njk";
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.markdownTemplateEngine = 'njk';
+  eleventyConfig.dataTemplateEngine = 'njk';
+  eleventyConfig.htmlTemplateEngine = 'njk';
+  eleventyConfig.addPlugin(pluginRss);
+
   eleventyConfig.addFilter('formatDate', function (value) {
     return format(value, 'yyyy-MM-dd');
   });
-}
+};
