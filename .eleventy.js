acfbc93e7fd32ebf766615925ac1afdb7ebc737d
Chad McElligott <chad.mcelligott@gmail.com>
2025-01-17 21:06:30 -0600

    Upgrade 11ty to v3

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
 const readingTime = require('eleventy-plugin-reading-time');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 const now = Date.now().toString();
 
 module.exports = function (eleventyConfig) {
